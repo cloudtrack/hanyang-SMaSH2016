ca2e22a4310abdcb0c777280db4c2e247bf482b0
huna3869 <huna3869@naver.com>
2016-01-23 18:49:12 +0900

    new file:   Day5-Homework/CList-jhlee/.idea/.name 	renamed:    Day5-Homework/CList-volley/.idea/compiler.xml -> Day5-Homework/CList-jhlee/.idea/compiler.xml 	renamed:    Day5-Homework/CList-volley/.idea/copyright/profiles_settings.xml -> Day5-Homework/CList-jhlee/.idea/copyright/profiles_settings.xml 	new file:   Day5-Homework/CList-jhlee/.idea/encodings.xml 	renamed:    Day5-Homework/CList-volley/.idea/gradle.xml -> Day5-Homework/CList-jhlee/.idea/gradle.xml 	renamed:    Day5-Homework/CList-volley/.idea/libraries/appcompat_v7_23_1_1.xml -> Day5-Homework/CList-jhlee/.idea/libraries/appcompat_v7_23_1_1.xml 	new file:   Day5-Homework/CList-jhlee/.idea/libraries/play_services_appindexing_8_1_0.xml 	new file:   Day5-Homework/CList-jhlee/.idea/libraries/play_services_basement_8_1_0.xml 	new file:   Day5-Homework/CList-jhlee/.idea/libraries/support_annotations_23_1_1.xml 	renamed:    Day5-Homework/CList-volley/.idea/libraries/support_v4_23_1_1.xml -> Day5-Homework/CList-jhlee/.idea/libraries/support_v4_23_1_1.xml 	renamed:    Day5-Homework/CList-volley/.idea/libraries/volley.xml -> Day5-Homework/CList-jhlee/.idea/libraries/volley.xml 	renamed:    Day5-Homework/CList-volley/.idea/misc.xml -> Day5-Homework/CList-jhlee/.idea/misc.xml

---
+++ Day5-Homework/CList-jhlee/app/src/main/java/info/androidhive/customlistviewvolley/MainActivity.java
@@ -0,0 +1,197 @@
+package info.androidhive.customlistviewvolley;
+
+import info.androidhive.customlistviewvolley.adater.CustomListAdapter;
+import info.androidhive.customlistviewvolley.app.AppController;
+import info.androidhive.customlistviewvolley.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends Activity {
+    // Log tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Movies json url
+    private static final String url = "http://androidsample.s3-website-ap-northeast-1.amazonaws.com/movies.json";
+    private ProgressDialog pDialog;
+    private List<Movie> movieList = new ArrayList<Movie>();
+    private ListView listView;
+    private CustomListAdapter adapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new CustomListAdapter(this, movieList);
+        listView.setAdapter(adapter);
+
+        pDialog = new ProgressDialog(this);
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        // changing action bar color
+        getActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#1b1b1b")));
+
+        // Creating volley request obj
+        JsonArrayRequest movieReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        hidePDialog();
+
+                        // Parsing json
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+
+                                JSONObject obj = response.getJSONObject(i);
+                                Movie movie = new Movie();
+                                movie.setTitle(obj.getString("title"));
+                                movie.setThumbnailUrl(obj.getString("image"));
+                                movie.setRating(((Number) obj.get("rating"))
+                                        .doubleValue());
+                                movie.setYear(obj.getInt("releaseYear"));
+
+                                // Genre is json array
+                                JSONArray genreArry = obj.getJSONArray("genre");
+                                ArrayList<String> genre = new ArrayList<String>();
+                                for (int j = 0; j < genreArry.length(); j++) {
+                                    genre.add((String) genreArry.get(j));
+                                }
+                                movie.setGenre(genre);
+
+                                // adding movie to movies array
+                                movieList.add(movie);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hidePDialog();
+
+            }
+        });
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
+                Movie m = movieList.get(position);
+                intent.putExtra("imageURL", m.getThumbnailUrl());
+                // 두번째 액티비티를 실행하기 위한 인텐트
+                startActivity(intent);
+
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hidePDialog();
+    }
+
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://info.androidhive.customlistviewvolley/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://info.androidhive.customlistviewvolley/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
